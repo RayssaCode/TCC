dd89b8a88b422ab3853b054c719f68a9397941ea
Rayssa Rodrigues <rayssa.rodrigues1198@gmail.com>
2022-06-01 18:44:26 -0300

    Colocar fonts

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,45 +1,45 @@
-import './index.scss';
+import "./index.scss";
 
+export default function Login() {
+  return (
+    <main className="page-login">
+      <section>
+        <div className="logo">
+          <img className="imagem" src="/images/odontotooths 1.svg" alt="" />
+          <a className="voltar" href="../home">Voltar</a>
+        </div>
+        <div className="destacar-login">
+          <div className="conteudo">
+            <div className="txt-icones">
+              <img src="/images/perfil 1 (1).svg" alt="perfil" />
+              <p>Nome*</p>
+            </div>
+            <div>
+              <input className="f-1" type="text" />
+            </div>
 
+            <div className="txt-icones">
+              <img src="/images/o-email 1.svg" alt="email" />
+              <p>E-mail*</p>
+            </div>
+            <div>
+              <input className="f-1" type="text" />
+            </div>
 
+            <div className="txt-icones">
+              <img src="/images/cadeado 1.svg" alt="cadeado" />
+              <p>Senha*</p>
+            </div>
+            <div>
+              <input className="f-1" type="password" />
+            </div>
 
-export default function Login(){
-    return(
-        <main className='page-login'>
-            <section>
-                <div className='logo'>
-                    <img className='imagem' src='/images/odontotooths 1.svg' alt=''></img>
-                    <a className='voltar' href='../home'>Voltar</a>
-                </div>
-                <div className='destacar-login'>
-                    <div className='conteudo'>
-                        <br/>
-                        <div className='logos'>
-                            <img src='' alt='perfil'>Nome*</img>
-                            <br/>
-                        </div>
-                        <input className='f-1' type='text'></input>
-                        <br/>
-                        <br/>
-                        <div className='logos'>
-                            <img src='' alt='email'>E-mail<br/></img>
-                        </div>
-                        <input className='f-1' type='text'></input>
-                        <div className='logos'>
-                            <br/>
-                            <img src='' alt='cadeado'>Senha*</img>
-                        </div>
-                        <input className='f-1' type='password'></input>
-                        <br/>
-                        <br/>
-                        <a href='../administrativo'>
-                            <button className='botao'>
-                                Entrar
-                            </button>
-                        </a>
-                    </div>
-                </div>
-            </section>
-        </main>
-    );
+            <a href="../administrativo">
+              <button className="botao">Entrar</button>
+            </a>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
